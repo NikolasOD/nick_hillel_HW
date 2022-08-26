e6d5908a5d7a74a078134d5dc664f06b3a8edee0
Bazan Nikolay <bazan.nick.art@gmail.com>
2022-08-26 11:56:27 +0300

    HW_8 Done

--- HW_8.py
+++ HW_8.py
@@ -4,7 +4,4 @@ my_dict = {}
 for word in src_string.lower().split():
     my_dict[word] = my_dict.get(word, 0) + 1
 
-# print(sorted(my_dict.items(), key=lambda x: (x[1], x[0]))[-1][0])
-# print(list(v for v in my_dict if my_dict[v] == max(my_dict.values()))[-1])
-# print({k: v for k, v in my_dict.items() if v == max(my_dict.values())})
-print(dict(filter(lambda x: x[1] == max(my_dict.values()), my_dict.items())).popitem()[0])
+print([v for v in my_dict if my_dict[v] == max(my_dict.values())][-1])
