2c18673d24e5c077db82ec51a0673983cbf19044
Bazan Nikolay <bazan.nick.art@gmail.com>
2022-08-29 13:20:58 +0300

    HW_7 Modified

--- HW_7.py
+++ HW_7.py
@@ -1,28 +1,30 @@
 class DigitalCounter:
     def __init__(self, start=0, end=100, current=None):
-        self.start = start
-        self.end = end
-        self.current = current
-        # Проверка текущего значения,
-        # должно быть в диапазоне между start и end
-        if self.current is not None:
-            if self.current < self.start:
+        if start < end:
+            self.start = start
+            self.end = end
+        else:
+            self.start = end
+            self.end = start
+
+        if current is not None:
+            if current < start:
                 raise Exception('Current value must be greater than start value')
             elif self.current > self.end:
                 raise Exception('Current value must be less than end value')
 
+        if current is None:
+            self.current = self.start
+        else:
+            self.current = current
+
     # Увеличение счётчика на 1
     def increase(self):
-        if self.current is not None:
-            if self.current < self.end:
-                self.current += 1
+        if self.current < self.end:
+            self.current += 1
         else:
-            self.current = self.start + 1
+            self.current = self.start
 
     # Вывод текущего значения счётчика в терминал
     def get_current_value(self):
-        # Я предположил что если текущее значение не задано,
-        # то следует выводить начальное
-        if self.current is None:
-            return self.start
         return self.current
